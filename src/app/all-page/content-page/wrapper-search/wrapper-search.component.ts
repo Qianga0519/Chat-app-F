34303e29e65cd7d1907fdab2f834631ed4ccd807
QuocDat <dathoa1104@gmail.com>
2024-10-24 23:36:02 +0700

    update

--- src/app/all-page/content-page/wrapper-search/wrapper-search.component.ts
+++ src/app/all-page/content-page/wrapper-search/wrapper-search.component.ts
@@ -1,19 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { UsersService } from '../../../service/dat/user.service';
+
+// Các component được import
 import { HeaderComponent } from "../../header/header.component";
 import { WrapperLeftComponent } from "../wrapper-left/wrapper-left.component";
 import { WrapperRightComponent } from "../wrapper-right/wrapper-right.component";
 import { ModalEditPostComponent } from "../../modal-edit-post/modal-edit-post.component";
 import { ModalCreatePostComponent } from "../../modal-create-post/modal-create-post.component";
 import { FooterComponent } from "../../footer/footer.component";
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-wrapper-search',
   standalone: true,
-  imports: [HttpClientModule,HeaderComponent, WrapperLeftComponent, WrapperRightComponent, ModalEditPostComponent, ModalCreatePostComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+    WrapperLeftComponent,
+    WrapperRightComponent,
+    ModalEditPostComponent,
+    ModalCreatePostComponent,
+    FooterComponent,
+  ],
   templateUrl: './wrapper-search.component.html',
-  styleUrl: './wrapper-search.component.css'
+  styleUrls: ['./wrapper-search.component.css']
 })
-export class WrapperSearchComponent {
+export class WrapperSearchComponent implements OnInit {
+  users: any[] = [];
+  searchKeyword: string = ''; // Biến lưu trữ từ khóa tìm kiếm
+  message: string = ''; // Biến lưu trữ thông báo
+  noUsersFound: boolean = false;
+  
+  constructor(private userService: UsersService) { }
+
+  ngOnInit(): void {
+   
+  }
+  showUsers: boolean = true;
+  showPosts: boolean = false;
+  searchUsers(keyword: string): void {
+    this.userService.searchUsers(keyword).subscribe(
+      (data) => {
+        // console.log('Dữ liệu người dùng:', data); // Thêm log để kiểm tra dữ liệu
+        if (Array.isArray(data)) {
+          this.users = data; // Lưu dữ liệu người dùng
+          this.noUsersFound = this.users.length === 0; // Kiểm tra xem có người dùng nào không
+        } else {
+          // console.error('Dữ liệu trả về không phải là mảng:', data);
+          this.users = []; // Gán giá trị rỗng để tránh lỗi *ngFor
+          this.noUsersFound = true; // Đánh dấu là không tìm thấy người dùng
+        }
+      },
+      (error) => {
+        // console.error('Có lỗi xảy ra!', error);
+        this.noUsersFound = true; // Có thể đánh dấu không tìm thấy nếu có lỗi
+      }
+    );
+  }
 
 }
