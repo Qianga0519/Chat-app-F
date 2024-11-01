2f1b545d01b89dc99071b9c3c04b7879cfb9bd2e
QuocDat <dathoa1104@gmail.com>
2024-11-01 23:54:38 +0700

    share

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { WrapperMessageComponent } from './all-page/content-page/wrapper-message
 import { WrapperSearchComponent } from './all-page/content-page/wrapper-search/wrapper-search.component';
 import { WrapperNotificationsComponent } from './all-page/content-page/wrapper-notifications/wrapper-notifications.component';
 import { AuthGuard } from './auth.guard';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -19,9 +20,11 @@ export const routes: Routes = [
     children: [
       { path: '', component: WrapperListPostComponent },
       { path: 'detail/:id', component: WrapperDetailPostComponent },
+      { path: 'user/:userId/detail/:postId', component: WrapperDetailPostComponent },
       { path: 'message', component: WrapperMessageComponent },
       { path: 'search', component: WrapperSearchComponent },
       { path: 'notifications', component: WrapperNotificationsComponent },
+      { path: '404', component: NotFoundComponent },
       {
         path: 'user',
         children: [
